05f50d06251d6069af7255c3dcd2de2f94ada617
Subhajeet Dey <dwij.stardust@gmail.com>
2014-11-28 00:25:02 +0530

    Schema for player table

--- app/database/migrations/2014_11_27_183604_create_players_table.php
+++ app/database/migrations/2014_11_27_183604_create_players_table.php
@@ -15,6 +15,8 @@ class CreatePlayersTable extends Migration {
 		Schema::create('players', function(Blueprint $table)
 		{
 			$table->increments('id');
+			$table->string('player_name');
+			$table->integer('points');
 			$table->timestamps();
 		});
 	}
