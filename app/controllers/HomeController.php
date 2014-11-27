21f348541e29c5ff3fc34fc62e43ed9daa756819
Subhajeet Dey <dwij.stardust@gmail.com>
2014-11-27 22:46:00 +0530

    Attempt Auth with username and password

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -19,11 +19,18 @@ class HomeController extends BaseController {
 
 	public function postLogin()
 	{
-		$user = User::find(1);
-		Auth::login($user);
+		$email = Input::get('email');
+		$password = Input::get('password');
+		if (Auth::attempt(array('email' => $email, 'password' => $password), true))
+		{
+			//log the user in and redirect to admin panel
+			return Redirect::to('admin');
 
-		//log the user in and redirect to admin panel
-		return Redirect::to('admin');
+		}else{
+
+			return View::make('login');
+		}
+		
 	}
 
 	public function postRegister()
