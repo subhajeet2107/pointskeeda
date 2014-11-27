fa6d17278567fe9c7f48e207f94a0664960fde2f
Subhajeet Dey <dwij.stardust@gmail.com>
2014-11-27 20:41:51 +0530

    Create Index View set home route

---
+++ app/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PointsKeeda</title>
+	<!-- Latest compiled and minified CSS & JS -->
+	<link rel="stylesheet" media="screen" href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	<script src="//code.jquery.com/jquery.js"></script>
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+</body>
+</html>
