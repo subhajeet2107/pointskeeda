68246f069b0c8ca7ca336baf34fb09ef22aa4172
Subhajeet Dey <dwij.stardust@gmail.com>
2014-11-28 02:01:13 +0530

    added new player create in admin

--- app/views/admin/index.blade.php
+++ app/views/admin/index.blade.php
@@ -6,6 +6,8 @@
     <link rel="stylesheet" media="screen" href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
     <script src="//code.jquery.com/jquery.js"></script>
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    <script src="http://templateplanet.info/tooltip.js"></script>
+    <script src="http://templateplanet.info/modal.js"></script>
 
     {{HTML::style('/font-awesome/css/font-awesome.min.css')}}
     {{ HTML::style('/css/style.css') }}
@@ -47,13 +49,15 @@
        @if (!empty($player_data) && count($player_data) > 0)
         <div class="panel panel-default">
             <div class="panel-body">
-               <table class="table table-bordered table-hover tablesorter">
+               <table class="table table-bordered table-hover tablesorter" id="tab_logic">
                     <thead>
                       <tr>
                       
                         <th>Id<i class="fa fa-sort"></i></th>
                         <th>Player Name<i class="fa fa-sort"></i></th>
                         <th>Player Points<i class="fa fa-sort"></i></th>
+                        <th>Edit</th>
+                        <th>Delete</th>
                       </tr>
                     </thead>
                     <tbody>
@@ -62,12 +66,15 @@
                             <td>{{ $player->id }}</td>
                             <td>{{ $player->player_name }}</td>
                             <td>{{ $player->points }}</td>
+                            <td><p><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" data-placement="top" rel="tooltip"><span class="glyphicon glyphicon-pencil"></span></button></p></td>
+                            <td><p><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" data-placement="top" rel="tooltip"><span class="glyphicon glyphicon-trash"></span></button></p></td>
                         </tr>
                         @endforeach
-                        
+                                                                  
                     </tbody>
                     
                   </table>
+                  <a id="add_row" class="btn btn-success pull-right" data-title="Add" data-toggle="modal" data-target="#add" data-placement="top">Add New Player</a>
             </div>
         </div>
     @else
@@ -119,8 +126,83 @@
     @endif
     </div>
 </div>
+
+<div class="modal fade" id="add" tabindex="-1" role="dialog" aria-labelledby="add" aria-hidden="true">
+      <div class="modal-dialog">
+    <div class="modal-content">
+          <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+        <h4 class="modal-title custom_align" id="Heading">Add New Player</h4>
+      </div>
+        <div class="modal-body">
+              <div class="form-group">
+                <input class="form-control" type="text">
+            </div>
+            <div class="form-group">
+            <input class="form-control" type="number">
+            </div>
+        </div>
+          <div class="modal-footer">
+        <button type="button" class="btn btn-warning btn-lg" style="width: 100%;"><span class="glyphicon glyphicon-ok-sign"></span> Add New Player</button>
+      </div>
+        </div>
+    <!-- /.modal-content --> 
+  </div>
+      <!-- /.modal-dialog --> 
+    </div>
+
+
+<div class="modal fade" id="edit" tabindex="-1" role="dialog" aria-labelledby="edit" aria-hidden="true">
+      <div class="modal-dialog">
+    <div class="modal-content">
+          <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+        <h4 class="modal-title custom_align" id="Heading">Edit Player Details</h4>
+      </div>
+        <div class="modal-body">
+              <div class="form-group">
+                <input class="form-control" type="text">
+            </div>
+            <div class="form-group">
+            <input class="form-control" type="number">
+            </div>
+        </div>
+          <div class="modal-footer">
+        <button type="button" class="btn btn-warning btn-lg" style="width: 100%;"><span class="glyphicon glyphicon-ok-sign"></span> Update</button>
+      </div>
+        </div>
+    <!-- /.modal-content --> 
+  </div>
+      <!-- /.modal-dialog --> 
+    </div>
+    
+    
+    
+    <div class="modal fade" id="delete" tabindex="-1" role="dialog" aria-labelledby="edit" aria-hidden="true">
+      <div class="modal-dialog">
+    <div class="modal-content">
+          <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+        <h4 class="modal-title custom_align" id="Heading">Delete this player ?</h4>
+      </div>
+          <div class="modal-body">
+       
+       <div class="alert alert-warning"><span class="glyphicon glyphicon-warning-sign"></span> Are you sure you want to delete this player?</div>
+       
+      </div>
+        <div class="modal-footer ">
+        <button type="button" class="btn btn-success" ><span class="glyphicon glyphicon-ok-sign"></span> Yes</button>
+        <button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> No</button>
+      </div>
+        </div>
+    <!-- /.modal-content --> 
+  </div>
+      <!-- /.modal-dialog --> 
+    </div>
+
 <!-- Page Specific Plugins -->
-{{HTML::script('/js/tablesorter/jquery.tablesorter.js')}}
-{{HTML::script('/js/tablesorter/tables.js')}}
+{{ HTML::script('/js/tablesorter/jquery.tablesorter.js') }}
+{{ HTML::script('/js/tablesorter/tables.js') }}
+{{ HTML::script('/js/main.js') }}
 </body>
 </html>
