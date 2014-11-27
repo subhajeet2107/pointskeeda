d6b401587f14596668c32b8161eff3b12b49752c
Subhajeet Dey <dwij.stardust@gmail.com>
2014-11-27 20:50:22 +0530

    Added BS3,Jquery,Created simple markup for homepage

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -6,8 +6,60 @@
 	<link rel="stylesheet" media="screen" href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 	<script src="//code.jquery.com/jquery.js"></script>
 	<script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+	<style>
+		@import url(//fonts.googleapis.com/css?family=Lato:700);
+
+		body {
+			margin:0;
+			font-family:'Lato', sans-serif;
+			text-align:center;
+			
+		}
+	</style>
+
 </head>
 <body>
+<div class="container">
+	<nav class="navbar navbar-default" role="navigation">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="{{ URL::to('/') }}">PointsKeeda</a>
+		</div>
+	
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse navbar-ex1-collapse">
+			<ul class="nav navbar-nav navbar-right">
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Admin Panel <b class="caret"></b></a>
+					<ul class="dropdown-menu">
+						<li><a href="#">Login</a></li>
+					</ul>
+				</li>
+			</ul>
+		</div><!-- /.navbar-collapse -->
+	</nav>
 
+	<div class="jumbotron">
+		<div class="container">
+			<h1>Welcome to PointsKeeda</h1>
+			<p>Simple app to show all players</p>
+			@if (empty($player_count))
+				<p>
+					No Players Found <a class="btn btn-primary btn-lg">Login to add players</a>
+				</p>
+			@endif
+			
+		</div>
+	</div>
+
+	
+</div>
 </body>
 </html>
